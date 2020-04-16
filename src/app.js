594beb481a72adaefd7a768605cd6b3abd365fcd
manugeorge04 <manugeorge004@gmail.com>
2020-04-16 18:01:27 +0530

    Add a index.html and a static call to it

--- src/app.js
+++ src/app.js
@@ -1,13 +1,38 @@
+const path = require('path')
 const express = require('express')
 
-const app = express()
+console.log(__dirname)
 
-app.get('', (req, res) => {                           //'' has nothing for the root page that is, app.com
-    res.send('Hello express')
+const app = express()
+const indexfile = path.join(__dirname, '../public')
+
+app.use(express.static(indexfile))
+
+app.get('', (req,res) =>{                               //'' has nothing for the root page that is, app.com
+    res.send('<h1>Weather<h1>')
 })
 
-app.get('/help', (req,res) =>{
-    res.send('Help Page')
+app.get('/help', (req, res) => {                          
+    res.send([{
+        name : 'Manu'
+    }, {
+        name : 'Jenu'
+    }])
+})
+
+app.get('/about', (req,res) =>{
+    res.send('<h2>About Page<h2>')
+})
+
+app.get('/weather', (req,res) =>{
+    res.send([
+        {
+            Forecast : 'Hot'
+        },
+        {
+            Location : 'Bangalore'
+        }
+    ])
 })
 
 app.listen(3000,() => {                               //port number 3000 is usually used for development callback is optional
